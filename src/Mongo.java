172f2cf8416c7176cdfaa4a8de74d21dc7d610cd
Manikandan Palanisamy <manikandanp@cdata.com>
2023-02-07 15:48:03 +0530

    pull everything

--- src/Mongo.java
+++ src/Mongo.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.InputStream;
+import java.util.Arrays;
+import java.sql.*;
+import java.util.Base64;
+public class Mongo {
+    public static void main(String[] args) throws SQLException {
+        //Registering the Driver
+        Connection connection;
+        connection = DriverManager.getConnection("jdbc:mongodb:Server=localhost;Database=Manitest;");
+        Statement stat = connection.createStatement();
+        String cmd = "INSERT INTO Manitest.Testdrill (buffer) VALUES (?)";
+        PreparedStatement pstmt = connection.prepareStatement(cmd);
+        String base64 = "46d989eaf0bde5258029534bc2dc2089";
+        byte[] binaryData = Base64.getDecoder().decode(base64);
+        pstmt.setBytes(1, binaryData);
+        int count = pstmt.executeUpdate();
+        System.out.println(count + " rows were affected");
+        connection.close();
     }
 }

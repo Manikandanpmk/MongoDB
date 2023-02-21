515320a85ee0df55f307c9c8fd2ac919b850ea1a
Manikandan Palanisamy <manikandanp@cdata.com>
2023-02-21 12:37:08 +0530

    new

--- src/mongodb/Mongo.java
+++ src/mongodb/Mongo.java
@@ -1,13 +1,22 @@
 package mongodb;
+
+import java.io.InputStream;
+import java.util.Arrays;
 import java.sql.*;
+import java.util.Base64;
 public class Mongo {
     public static void main(String[] args) throws SQLException {
+//Registering the Driver
         Connection connection;
         connection = DriverManager.getConnection("jdbc:mongodb:Server=localhost;Database=Manitest;");
         Statement stat = connection.createStatement();
         CallableStatement cstmt = connection.prepareCall("AddDocument");
-        cstmt.setString("collection", "dummy");
-        cstmt.setString("document", "{_id:\"some-id\",buffer:\"hello\"}");
+        cstmt.setString("collection", "Newmongodb");
+        cstmt.setString("document", "{\"buffer\": {\n" +
+                " \"$binary\": {\n" +
+                " \"base64\": \"46d989eaf0bde5258029534bc2dc2089\",\n" +
+                " \"subType\": \"00\"\n" +
+                " }}}");
         boolean ret = cstmt.execute();
         if (ret) {
             int count = cstmt.getUpdateCount();
